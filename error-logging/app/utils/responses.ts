a2eb05dbde918ddad9dd9fcc7fe3b5f73dacf050
kiliman <kiliman@gmail.com>
2022-11-20 23:07:21 -0500

    🔨 Clean up response functions and types

--- error-logging/app/utils/responses.ts
+++ error-logging/app/utils/responses.ts
@@ -1,4 +1,4 @@
-import { type ActionData, type ErrorsType } from '~/utils/data'
+import { type ActionData } from '~/utils/data'
 import { typedjson } from './typedjson'
 
 export function notFound(message?: string) {
@@ -17,31 +17,15 @@ export function forbidden(message?: string) {
   return new Response(message ?? 'Unauthorized', { status: 403 })
 }
 
-type FieldsType = { [key: string]: string }
-export function invalid(
-  data: { errors: ErrorsType; fields: FieldsType },
-  init?: number | ResponseInit,
-) {
-  return typedjson({ errors } as ActionData<unknown>, init ?? { status: 400 })
+export function invalid<T>(data: T) {
+  return typedjson(data, { status: 400 })
 }
-
-// export function invalid<FieldsType>(
-//   errors: ErrorsType,
-//   fields?: FieldsType,
-//   init?: number | ResponseInit,
-// ) {
-//   return json(
-//     { errors, fields } as ActionData<unknown, FieldsType>,
-//     init ?? { status: 400 },
-//   )
-// }
-
 export function success<SuccessType>(
   data: SuccessType,
   init?: number | ResponseInit,
 ) {
   return typedjson(
-    { success: data } as ActionData<SuccessType>,
+    { success: data } as ActionData<SuccessType, unknown>,
     init ?? { status: 200 },
   )
 }
